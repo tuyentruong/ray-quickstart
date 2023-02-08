ff82635b3fc005ad4abf268e7cb6bcfef2f1a6c9
Tuyen Truong <tuyen.truong@gmail.com>
2023-02-08 04:30:48 -0800

    Setting to the venv directory as calculated by PyCharm so that PyCharm can share the same venv

--- setup/setup_mac.sh
+++ setup/setup_mac.sh
@@ -12,8 +12,9 @@ source /opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh
 conda activate ray-quickstart
 
 conda env config vars set PIPENV_PIPFILE=~/git/ray-quickstart/Pipfile.mac
+conda env config vars set PIPENV_CUSTOM_VENV_NAME=ray-quickstart-TShZKXXf # setting to the venv directory as calculated by PyCharm so that PyCharm can share the same venv
 
-conda install pipenv -y
+conda install pipenv twine -y
 
 # install conda version to get version of grpc that works with Macs
 conda install grpcio=1.43.0 -y
