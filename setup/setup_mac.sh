257f0c4cc6973e42168436503498c989c868ea31
Tuyen Truong <tuyen.truong@gmail.com>
2023-02-09 21:24:17 -0800

    Fixed installing Tensorflow on Mac Apple Silicon

--- setup/setup_mac.sh
+++ setup/setup_mac.sh
@@ -14,6 +14,7 @@ conda activate ray-quickstart
 conda env config vars set PIPENV_PIPFILE=~/git/ray-quickstart/Pipfile.mac
 conda env config vars set PIPENV_CUSTOM_VENV_NAME=ray-quickstart-TShZKXXf # setting to the venv directory as calculated by PyCharm so that PyCharm can share the same venv
 
+conda install -c apple tensorflow-deps -y
 conda install pipenv twine -y
 
 # install conda version to get version of grpc that works with Macs
